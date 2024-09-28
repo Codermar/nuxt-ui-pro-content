3b35fd8b875077b9b6e3684d3dc8b4485db0949c
Jose Alfonso <jose.alfonso@apple.com>
2024-09-28 11:28:58 -0700

    feat: content catch all

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,12 @@
+<template>
+  <article>
+    <ContentDoc v-slot="{ doc }">
+      <h1 class="text-xl heading-1">
+        {{ doc.title }}
+      </h1>
+      <ContentRenderer
+        :value="doc"
+      />
+    </ContentDoc>
+  </article>
+</template>
