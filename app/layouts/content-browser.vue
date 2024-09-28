93a6fad66ca73458485ca5557261081a053e3b32
Jose Alfonso <jose.alfonso@apple.com>
2024-09-28 11:29:41 -0700

    feat: content browser page + layout

---
+++ app/layouts/content-browser.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <!-- Left Navigation -->
+    <nav class="min-h-screen w-64 bg-gray-100 px-4">
+      <h2 class="py-0 text-base heading-2">
+        Content
+      </h2>
+      <ContentNavigation v-slot="{ navigation }">
+        <UAside>
+          <UNavigationTree
+            :links="mapContentNavigation(navigation)"
+          />
+        </UAside>
+        <!-- <ul>
+          <li v-for="link of navigation" :key="link._path">
+            <NuxtLink :to="link._path" class="block py-2">
+              {{ link.title }}
+            </NuxtLink>
+          </li>
+        </ul> -->
+      </ContentNavigation>
+    </nav>
+
+    <!-- Main Content -->
+    <main class="flex-grow p-4">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ app/pages/content-browser.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'content-browser',
+})
+</script>
+
+<template>
+  <h1 class="mt-0 heading-3">
+    Content Browser
+  </h1>
+</template>
